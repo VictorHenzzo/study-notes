e52984743ca977da5519cf633ae45a4fa29673c8
Victor <victorhenzzowp@gmail.com>
2021-10-06 11:13:57 -0300

    Starting bank project

---
+++ Dart/bank/bin/bank.dart
@@ -0,0 +1,5 @@
+import 'package:bank/bank.dart' as bank;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bank.calculate()}!');
+}
---
+++ Dart/bank/test/bank_test.dart
@@ -0,0 +1,8 @@
+import 'package:bank/bank.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
