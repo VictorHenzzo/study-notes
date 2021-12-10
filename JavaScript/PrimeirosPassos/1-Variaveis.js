f3f0be62df11e268ae190b760f4683316bbc6ae9
Victor <victorhenzzowp@gmail.com>
2021-12-09 23:35:25 -0300

    Introdução ao JS

---
+++ JavaScript/PrimeirosPassos/1-Variaveis.js
@@ -0,0 +1,17 @@
+console.log("Meu primeiro programa em JS: Variáveis");
+
+let idade = 22;
+const nome = "Victor";
+console.log(idade);
+
+idade++;
+console.log("Nome: ", nome, "Idade: ", idade);
+
+//Operações aritméticas
+const valor1 = 12;
+const valor2 = 5;
+console.log("Soma: ", valor1 + valor2);
+console.log("Subtração: ", valor1 - valor2);
+console.log("Multiplicação: ", valor1 * valor2);
+console.log("Divisão: ", valor1 / valor2);
+console.log("Resti: ", valor1 % valor2);
---
+++ JavaScript/PrimeirosPassos/3-AtribuicaoImutabilidade.js
@@ -0,0 +1,5 @@
+const idade = 22;
+const nome = "Victor";
+const sobrenome = "Henzzo";
+
+console.log(`Meu nome é ${nome} ${sobrenome}`);
