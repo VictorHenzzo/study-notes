8397e6f30c8c7d4bc8de2ef43e80c7f35206913d
Victor <victorhenzzowp@gmail.com>
2022-03-03 19:51:04 -0300

    JS lectures

---
+++ JavaScript/js_back_end_Alura/capitulo_2/var_let_const.js
@@ -0,0 +1,34 @@
+// var
+
+// var altura = 3;
+// var comprimento = 5;
+// var area = altura * comprimento;
+// console.log(area);
+
+// let
+
+// let forma = 'retangulo'
+// let altura = 3;
+// let comprimento = 5;
+// let area;
+// if (forma === 'retangulo') {
+//    area = altura * comprimento;
+// } else {
+//    area = (altura * comprimento) / 2;
+// }
+
+// console.log(area);
+
+// const
+
+const forma = 'triangulo'
+const altura = 5;
+const comprimento = 7;
+//const area; -> Precisa ser inicializado, não pode ser alterado
+let area;
+if (forma === 'retangulo') {
+   area = altura * comprimento;
+} else {
+   area = (altura * comprimento) / 2;
+}
+console.log(area);
