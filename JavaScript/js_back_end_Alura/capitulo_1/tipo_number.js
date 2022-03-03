b9c5cc2d917155949f1a1969c72efc8131e5fbaf
Victor <victorhenzzowp@gmail.com>
2022-03-03 19:17:32 -0300

    JS lectures

---
+++ JavaScript/js_back_end_Alura/capitulo_1/tipo_number.js
@@ -0,0 +1,21 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNum = 1;
+const segundoNum = 2;
+
+const operacaoMatematica = primeiroNum + segundoNum;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numPontoFlutuante = 3.3;
+const numPontoFlutuante2 = .7;
+
+console.log(numPontoFlutuante / numPontoFlutuante2);
+
+// NaN -> not a number
+
+console.log("hllwrld" / meuNumero);
---
+++ JavaScript/js_back_end_Alura/capitulo_1/tipo_string.js
@@ -0,0 +1,12 @@
+// type string
+const texto1 = 'La mort ne veut rien dire';
+console.log(texto1);
+
+const citacao = 'Ele disse "Já já eu chego"';
+console.log(citacao);
+
+// concatenação
+
+const nome = 'Victor';
+const sobrenome = 'Henzzo';
+console.log(nome + ' ' + sobrenome);
