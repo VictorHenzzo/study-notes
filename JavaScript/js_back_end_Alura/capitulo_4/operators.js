7028d9df55fd3202f089ca1080a92119dc55aded
Victor H <victorhenzzowp@gmail.com>
2023-06-24 13:45:35 -0300

    Finishing chapter 4

--- JavaScript/js_back_end_Alura/capitulo_4/operators.js
+++ JavaScript/js_back_end_Alura/capitulo_4/operators.js
@@ -3,3 +3,8 @@ const text = '5';
 
 console.log(number==text);
 console.log(number===text);
+
+const minimalAge = 18;
+const clientAge = 15;
+
+console.log(clientAge >= minimalAge ? 'Can drive': 'Cannot drive!');
